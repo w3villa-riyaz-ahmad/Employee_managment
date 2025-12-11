98f098f8031dafabf06e972deb75860502f691af
Riyaz Ahmad <riyaz.ahmad@w3villa.com>
2025-12-11 19:02:29 +0530

    add devise and modifide it forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users
+      devise_for :users, controllers: {
+        sessions: "users/sessions",
+        registrations: "users/registrations",
+        passwords: "users/passwords",
+        confirmations: "users/confirmations"
+      }
+
   get "up" => "rails/health#show", as: :rails_health_check
-  
+
   # RESTful routes for employees
   resources :employees
   resources :documents
-  
-  
-  root 'employees#index'
+
+
+  root "employees#index"
 end
